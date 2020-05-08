7e11bb7f40d66b46a1f90a7673f18696e1451802
bzzt lost a hitlab login <bzzt@bzzt.bzzt>
2020-05-08 13:37:00 +0000

    avoid blocking on pagerebuild
    Signed-off-by: Bret Curtis <psi29a@gmail.com>

--- apps/openmw/mwworld/worldimp.cpp
+++ apps/openmw/mwworld/worldimp.cpp
@@ -837,7 +837,8 @@ namespace MWWorld
             if (reference.getCellRef().getRefNum().hasContentFile())
             {
                 int type = mStore.find(Misc::StringUtils::lowerCase(reference.getCellRef().getRefId()));
-                mRendering->pagingEnableObject(type, reference, true);
+                if (mRendering->pagingEnableObject(type, reference, true))
+                    mWorldScene->reloadTerrain();
             }
         }
     }
@@ -878,7 +879,8 @@ namespace MWWorld
         if (reference.getCellRef().getRefNum().hasContentFile())
         {
             int type = mStore.find(Misc::StringUtils::lowerCase(reference.getCellRef().getRefId()));
-            mRendering->pagingEnableObject(type, reference, false);
+            if (mRendering->pagingEnableObject(type, reference, false))
+                mWorldScene->reloadTerrain();
         }
 
         if(mWorldScene->getActiveCells().find (reference.getCell())!=mWorldScene->getActiveCells().end() && reference.getRefData().getCount())
